3f0f4d15c2d1edf933b0e6a08a220abb6be73ab3
rohitcodism <rohitpaula18i@gmail.com>
2024-03-21 23:46:46 +0530

    ✅ Add Spotify Web API library

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-import './App.css'
-import Login from './components/login'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/login";
+import { getAccessTokenFromUrl } from "../server/utils/ListenIt";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(" ");
+
+  useEffect(() => {
+    const token = getAccessTokenFromUrl();
+    window.location.hash = "";
+    const accessToken = token?.access_token;
+    console.log("I have a token.", accessToken);
+
+    if (accessToken){
+      setToken(accessToken);
+      spotify.setAccessToken(accessToken);
+      spotify.getMe().then(user => {
+        user ? console.log(`User : `,user) : ("did not get anything!!!");
+      })
+    }
+  }, []);
 
   return (
     <div
       style={{
-        width : "100vw",
-        height : "100vh",
-        backgroundColor : "#000000"
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#000000",
       }}
     >
-        {/* Logo */}
-
-        {/* Login Screen */}
-        <Login />
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
